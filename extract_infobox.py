1c8edc7dacd4716e077470fae8c1876e6611f5e7
Ian <ianleaman1@gmail.com>
2015-04-08 15:12:11 -0400

    cleaning up @jkron

--- extract_infobox.py
+++ extract_infobox.py
@@ -11,7 +11,6 @@ bracketSum = 0
 i = 0
 for line in f:
     line = line.decode("utf-8")
-    # print(line)
     if '<page>' in line:
         onPage = True
     elif '</page>' in line:
@@ -24,20 +23,19 @@ for line in f:
             # Spin off to function extract_box_subject
             # + account for other cases
             boxSubject = line.strip("{{Infobox").strip(" ")
-            # print(boxSubject)
             # Skip info box subject and move on to processing
             # The next line
+            # continue
 
         if onInfoBox:
             bracketSum += line.count("{{") - line.count("}}")
             box += line
             # Exit the infobox
             if bracketSum == 0:
-                print(box.encode("utf-8"))
+                print(box.encode("utf-8").decode())
                 onInfoBox = False
                 i += 1
-                if i == 100000:
-                    break
+                break
             # Error Checking
             elif brackLevel < 0:
                 raise Exception("Something went wrong in "
